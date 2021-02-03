7a34c8a022ee8192b7c1d7af788577938d49de9d
tkotowoda <tkotowoda@gmail.com>
2021-02-03 14:08:28 +0100

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,47 +8,47 @@ namespace unittests
     [TestClass]
     public class UnitTest1
     {
-        [Test]
+        [TestMethod]
         public void GetItems()
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest("users", Method.GET);
+            var request = new RestRequest("/users{id}", Method.GET);
             var response = client.Execute(request);
             Console.WriteLine(response.Content);
         }
-        [Test]
+        [TestMethod]
         public void GetItems2(string filt)
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest("users", Method.GET);
+            var request = new RestRequest("/users", Method.GET);
             request.AddParameter("filter", filt);
             var response = client.Execute(request);
             Console.WriteLine(response.Content);
         }
-        [Test]
+        [TestMethod]
         private void PostItem(string data)
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest("users", Method.POST);
+            var request = new RestRequest("/users", Method.POST);
             request.AddParameter("data", data);
             var response = client.Execute(request);
             Console.WriteLine(response.Content);
         }
-        [Test]
+        [TestMethod]
         private static void PutItem(int id, string data)
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest("users", Method.PUT);
+            var request = new RestRequest("/users", Method.PUT);
             request.AddParameter("id", id);
             request.AddParameter("data", data);
             var response = client.Execute(request);
             Console.WriteLine(response.Content);
         }
-        [Test]
+        [TestMethod]
         private static void DeleteItem(int id)
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest($"users/{id}", Method.DELETE);
+            var request = new RestRequest($"/users/{id}", Method.DELETE);
             var response = client.Execute(request);
             Console.WriteLine(response.Content);
         }
